709bc88f12d492b7f850e3ad58f1778ac2f19653
Michael Maleki <contact@michaelmaleki.com>
2019-08-30 17:11:16 -0400

    email functional thanks to dale

--- server/server.js
+++ server/server.js
@@ -1,29 +1,23 @@
-// express config
 const express = require('express');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
+
 const fs = require('fs');
 const nanoid = require('nanoid');
-const multer = require('multer');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const path = require("path");
 
-const app = express();
-
 const routes = require('./routes/routes.js');
-const myurl = 'mongodb+srv://lesspensive:Tidefire11@lesspensive-scizd.mongodb.net/test?retryWrites=true&w=majority';
-const router = express.Router();
 
-
-// express middleware config
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// middleware
-
-//-------------------------------------------------
+//------------------------------------------------- MULTER STORAGE
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -39,20 +33,7 @@ var upload = multer({
   limits: { fileSize: 1000000 },
 }).single("myFile");
 
-
-// const storage = multer.diskStorage({
-//     destination: "./routes/uploads/",
-//     filename: function(req, file, cb){
-//        cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
-//     }
-//  });
-
-// const upload = multer({
-//     storage: storage,
-//     limits:{fileSize: 1000000},
-//  }).single("myImage");
-
-
+//------------------------------------------------- AXIOS POST REQUEST
 
 app.post('/upload', (req, res) => {
   upload(req, res, function (err) {
@@ -60,13 +41,65 @@ app.post('/upload', (req, res) => {
     console.log("Request file ---", req.file);
     /*Now do whatever you want to do*/
 
-    //
+    var img = fs.readFileSync(req.file.path);
+
+    var encode_image = img.toString("base64");
+
+
+    main(encode_image).catch(console.error);
 
     if (!err) {
-      return res.send(200).end();
+      return res.sendStatus(200).end();
     }
   })
 })
+
+//------------------------------------------------- NODEMAILER
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(photo) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    auth: {
+      user: "maleki.webdev@gmail.com",
+      pass: "lnermzumajbfvyyg"
+    }
+  });
+
+  // console.log(__dirname)
+
+  Attachement = {
+
+    filename: 'test.png',
+    content: photo,
+    encoding: 'base64' // optional, would be detected from the filename
+  }
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Lesspensive.com" <www.lesspensive.com>', // sender address
+    to: 'contact@lesspensive.com', // list of receivers
+    subject: 'Lesspensive Room Request', // Subject line
+    text: 'A request has been sent by <a user>', // plain text body
+    html: '<b>A request has been sent by a user</b>', // html body
+    attachments: Attachement
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+//------------------------------------------------- PORT LISTENER
 
 app.listen(1337, () => {
   console.log('Server is operational on port 1337.');
